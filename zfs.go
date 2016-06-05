32188f097d7beb642fc80d20619a4fb8a1afa72d
Clint Armstrong <clint@clintarmstrong.net>
2016-06-04 22:32:30 -0400

    create root

---
+++ zfs.go
@@ -0,0 +1,62 @@
+package zfs
+
+import(
+	"os/exec"
+	"os"
+	"io"
+	log "github.com/Sirupsen/logrus"
+)
+
+var(
+	zfsExec	string
+)
+
+type dataset struct{
+}
+
+func init() {
+	var err error
+	zfsExec, err = exec.LookPath("zfs")
+	if err != nil {
+		log.Error(err)
+		log.Fatal("zfs Executable not found")
+	}
+}
+
+func DatasetExists(dsName string) bool {
+	err := exec.Command(zfsExec, "list", dsName).Run()
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func GetMountPoint(dsName string) (string, error) {
+	out, err := exec.Command(	zfsExec, "get", "-H",
+								"-o", "value",
+								"mountpoint", dsName	).Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func CreateDatasetMount(dsName string, mountpoint string) error {
+	return exec.Command(	zfsExec, "create", 
+							"-o", "mountpoint="+mountpoint,
+							dsName							).Run()
+}
+
+func IsEmpty(name string) (bool, error) {
+    f, err := os.Open(name)
+    if err != nil {
+        return false, err
+    }
+    defer f.Close()
+
+    _, err = f.Readdirnames(1) // Or f.Readdir(1)
+    if err == io.EOF {
+        return true, nil
+    }
+    return false, err // Either not empty or error, suits both cases
+}
