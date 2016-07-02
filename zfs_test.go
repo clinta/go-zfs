aac6a6c073f58014becf63aa93e23594eba65f37
Clint Armstrong <clint@clintarmstrong.net>
2016-07-02 15:02:41 -0400

    zfs test wtf

--- zfs_test.go
+++ zfs_test.go
@@ -154,27 +154,37 @@ func TestCreateDatasetMount(t *testing.T) {
 }
 
 func TestCloneDataset(t *testing.T) {
-	assert := assert.New(t)
-
 	ds, err := CreateDataset("sjlTestPool/TestCloneSrc", nil)
-	assert.NoError(err)
-	defer assert.NoError(ds.Destroy())
+	if err != nil {
+		t.Error(err)
+	}
+	defer ds.Destroy()
 
 	d1 := []byte("test\nfile\n")
 	err = ioutil.WriteFile("/sjlTestPool/TestCloneSrc/testfile", d1, 0644)
-	assert.NoError(err)
+	if err != nil {
+		t.Error(err)
+	}
 
 	sn, err := ds.Snapshot("0")
-	assert.NoError(err)
+	if err != nil {
+		t.Error(err)
+	}
 
 	dsc, err := sn.Clone("sjlTestPool/TestCloneDst")
-	assert.NoError(err)
-	defer assert.NoError(dsc.Destroy())
+	if err != nil {
+		t.Error(err)
+	}
+	defer dsc.Destroy()
 
 	empty, err := IsEmpty("/sjlTestPool/TestCloneDst")
-	assert.NoError(err)
+	if err != nil {
+		t.Error(err)
+	}
 
-	assert.False(empty, "Cloned dataset is empty")
+	if empty {
+		t.Error("Cloned dataset is empty")
+	}
 }
 
 func TestPromoteDataset(t *testing.T) {
